5b62c7a7118962f78f49fbb76b848002202690c6
Yohannis <yohannisadmasu05@gmail.com>
2026-01-07 16:13:44 +0400

    add new changes

--- Area.java
+++ Area.java
@@ -1,6 +1,6 @@
 public class Area{
     public static void main(String[] args) {
-        System.out.println("Area of Circle: " + areaOfCircle(5));
+        System.out.println("Feature order change " + areaOfCircle(5));
     }
     public static double areaOfCircle(double radius) {
         return Math.PI * radius * radius;
